d4029344b3f840a156b687974ec8bfa9521b8ffa
Snoword <1430750200@qq.com>
2022-07-14 16:31:48 +0800

    feat: add dark toggle

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import Clock from './components/Clock.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
   <Clock />
+  <Footer flex-grow-0 text-center mb-6 />
   <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
   <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
 </template>
